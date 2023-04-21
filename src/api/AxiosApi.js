a8cc138b64300da254011c7894cb200a32762e16
jks2024 <jks2024@gmail.com>
2023-04-21 18:18:50 +0900

    회원가입 페이지 추가

--- src/api/AxiosApi.js
+++ src/api/AxiosApi.js
@@ -7,12 +7,38 @@ const AxiosApi = {
         const login = {
             id : id,
             pwd : pw 
-        }
+        };
         return await axios.post(KH_DOMAIN + "/login", login);
     },
     // 회원 조회
     memberGet: async(id) => {
         return await axios.get(KH_DOMAIN +`/member?id=${id}`);
     },
+    // 회원 가입
+    memberReg: async(id, pwd, name, mail) => {
+        const member = {
+            id: id, 
+            pwd: pwd,
+            name: name,
+            mail: mail
+        };
+        return await axios.post(KH_DOMAIN + "/new", member);
+    },
+    // 회원 가입 여부 확인
+    memberRegCheck: async(id) => {
+        const check = {
+            id: id
+        };
+        return await axios.post(KH_DOMAIN + "/check", check);
+    },
+
+    // 회원 탈퇴
+    memberDel: async(id) => {
+        const del = {
+            id: id
+        };
+        return await axios.post(KH_DOMAIN + "/delete", del);
+    }
+
 };
 export default AxiosApi;
--- src/signup/Signup.js
+++ src/signup/Signup.js
@@ -1,10 +1,108 @@
 import React, { useState } from 'react';
+import {useNavigate} from "react-router-dom";
+import Modal from '../utils/Modal';
+import AxiosApi from '../api/AxiosApi';
 
 const Signup = () =>{
+     // 키보드 입력
+     const [inputId, setInputId] = useState("");
+     const [inputPw, setInputPw] = useState("");
+     const [inputConPw, setInputConPw] = useState("");
+     const [inputName, setInputName] = useState("");
+     const [inputEmail, setInputEmail] = useState("");
+ 
+     // 오류 메시지
+     const [idMessage, setIdMessage] = useState("");
+     const [pwMessage, setPwMessage] = useState("");
+     const [conPwMessage, setConPwMessage] = useState("");
+     const [mailMessage, setMailMessage] = useState("");
+ 
+     // 유효성 검사
+     const [isId, setIsId] = useState(false);
+     const [isPw, setIsPw] = useState(false)
+     const [isConPw, setIsConPw] = useState(false);
+     const [isName, setIsName] = useState(false);
+     const [isMail, setIsMail] = useState(false);
+     // 팝업
+     const [modalOpen, setModalOpen] = useState(false);
+     const [modalText, setModelText] = useState("중복된 아이디 입니다.");
+
+     const closeModal = () => {
+        setModalOpen(false);
+    };
+
+    const onChangId = (e) => {
+        setInputId(e.target.value)
+        if (e.target.value.length < 5 || e.target.value.length > 12) {
+            setIdMessage("5자리 이상 12자리 미만으로 입력해 주세요.");
+            setIsId(false);    
+        } else {
+            setIdMessage("올바른 형식 입니다.");
+            setIsId(true);
+        }
+    }
+    const onChangePw = (e) => {
+        //const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/
+        const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/
+        const passwordCurrent = e.target.value ;
+        setInputPw(passwordCurrent);
+        if (!passwordRegex.test(passwordCurrent)) {
+            setPwMessage('숫자+영문자 조합으로 8자리 이상 입력해주세요!')
+            setIsPw(false)
+        } else {
+            setPwMessage('안전한 비밀번호에요 : )')
+            setIsPw(true);
+        }        
+    }
+    const onChangeConPw = (e) => {
+        const passwordCurrent = e.target.value ;
+        setInputConPw(passwordCurrent)
+        if (passwordCurrent !== inputPw) {
+            setConPwMessage('비밀 번호가 일치하지 않습니다.')
+            setIsConPw(false)
+        } else {
+            setConPwMessage('비밀 번호가 일치 합니다. )')
+            setIsConPw(true);
+        }      
+    }
+    const onChangeName = (e) => {
+        setInputName(e.target.value);
+        setIsName(true);
+    }
+    const onChangeMail = (e) => {
+        setInputEmail(e.target.value);
+        setIsMail(true);
+    }
+
+    const onClickLogin = async() => {
+        console.log("Click 회원가입");
+        // 가입 여부 우선 확인
+        const memberCheck = await AxiosApi.memberRegCheck(inputId);
+        console.log("가입 가능 여부 확인 : ", memberCheck.data);
+        // 가입 여부 확인 후 가입 절차 진행
+
+        if (memberCheck.data.result === "OK") {
+            console.log("가입된 아이디가 없습니다. 다음 단계 진행 합니다.");
+            const memberReg = await AxiosApi.memberReg(inputId, inputPw, inputName, inputEmail);
+            console.log(memberReg.data.result);
+            if(memberReg.data.result === "OK") {
+                window.location.replace("/");
+            } else {
+                setModalOpen(true);
+                setModelText("회원 가입에 실패 했습니다.");
+            }
+
+        } else {
+            console.log("이미 가입된 회원 입니다.")
+            setModalOpen(true);
+            setModelText("이미 가입된 회원 입니다.");
+        } 
+    }
+
+
 
      return(
         <>
-            <p>회원 가입 페이지 입니다.</p>
         </>
 
      );
