10775220fa75e62311853cfb515cc6c7ee43b253
jks2024 <jks2024@gmail.com>
2023-11-29 10:03:07 +0900

    셋팅메뉴수정

--- src/pages/setting/Setting.jsx
+++ src/pages/setting/Setting.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { FaChevronRight } from "react-icons/fa";
 import Common from "../../utils/Common";
@@ -44,9 +44,10 @@ const LinkStyled = styled(Link)`
 `;
 
 const Setting = () => {
+  const navigate = useNavigate();
   const handleLogout = () => {
     localStorage.removeItem("accessToken");
-    window.location.href = "/";
+    navigate("/");
   };
 
   return (
